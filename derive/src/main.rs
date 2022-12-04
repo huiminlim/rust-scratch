3709f6a1240bc6033411e9b5cf6323aab6d0bf60
huiminlim <e0036379@u.nus.edu>
2022-12-04 21:20:53 +0800

    Add derive functionality

---
+++ derive/src/main.rs
@@ -0,0 +1,20 @@
+// Derive debug printing functionality
+#[derive(Debug, Clone, Copy)] // Ownership of object is not changed, as clone/copy is used
+enum Position {
+    Manager,
+    Supervisor,
+    Worker,
+}
+#[derive(Debug, Clone, Copy)] // Ownership of object is not changed, as clone/copy is used
+struct Employee {
+    position: Position,
+    work_hours: i32,
+}
+
+fn main() {
+    let me = Employee {
+        position: Position::Manager,
+        work_hours: 40,
+    };
+    println!("{:?}", me);
+}

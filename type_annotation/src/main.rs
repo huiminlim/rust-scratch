d824714cb6c30fafbf5313f62aa5f52c6cb9a75f
huiminlim <e0036379@u.nus.edu>
2022-12-04 21:29:37 +0800

    Add type annotation examples

---
+++ type_annotation/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    // Annotate that the variable type is int32
+    let num: i32 = 14;
+    let a: char = 'a';
+
+    // Generics
+    let numbers: Vec<i32> = vec![1, 2, 3];
+}

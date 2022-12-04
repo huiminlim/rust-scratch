05487bc8a0e03ea6e4b86b3762958b21d9d0a552
huiminlim <e0036379@u.nus.edu>
2022-12-04 16:09:01 +0800

    Add basic functions, print code

--- beginning/src/main.rs
+++ beginning/src/main.rs
@@ -1,3 +1,11 @@
+fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
 fn main() {
     println!("Hello, world!");
+
+    let sum = 2 + 1;
+    let value = sub(10, 2);
+
+    println!("Hello, {} {}", sum, value);
 }

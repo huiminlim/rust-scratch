7e7b2ea40dd2bec4b29f9828a2b593ac9d3fde36
huiminlim <e0036379@u.nus.edu>
2022-12-07 17:04:20 +0800

    Add more examples of iter

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -5,6 +5,14 @@ fn main() {
 
     // Use iter() to iterate through every number, perform some map fn, then collect
     let plus_one: Vec<i32> = numbers.iter().map(|num| num + 1).collect();
-
     dbg!(plus_one);
+
+    // Use iter() to filter out elements that match some criteria
+    let new_numbers: Vec<_> = numbers.iter().filter(|num| **num > 3).collect();
+    dbg!(new_numbers);
+
+    // Use iter() to find element that match some criteria\
+    // Return none if not found
+    let find_me: Option<_> = numbers.iter().find(|num| **num > 23);
+    dbg!(find_me);
 }

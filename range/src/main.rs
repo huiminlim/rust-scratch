dfa7861e4face5000b063208ae58d475e564ae5a
huiminlim <e0036379@u.nus.edu>
2022-12-07 17:06:47 +0800

    Add range

---
+++ range/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    // Create values 1 to 3 inclusive
+    let range = 'a'..='z';
+    dbg!(range);
+
+    // Create values 1 to 4 not-inclusive
+    let range2 = 1..4;
+    dbg!(range2);
+}
